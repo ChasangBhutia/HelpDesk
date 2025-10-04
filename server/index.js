190014a4bd885a8e22bb70a3ea82b7111e57b065
ChasangBhutia <chasangtsering@gmail.com>
2025-10-04 19:30:49 +0530

    Initial setup: React + Node

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is live on PORT: ${PORT}`)
+})
